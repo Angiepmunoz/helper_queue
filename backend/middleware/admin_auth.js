cbca9cf192335e038f317c5bc2683981e6dc828a
Angie Munoz <angiepmunoz@gmail.com>
2021-06-11 14:06:14 -0400

    included api keys in frontend and backend

--- backend/middleware/admin_auth.js
+++ backend/middleware/admin_auth.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const checkAdminId = async (req, res, next) => {
     const admin_id = req.user.id;
+    console.log(admin_id);
     if (admin_id !== process.env.SECRET_ADMIN_KEY) {
         res.status(401).json({ message: "Must be admin!" });
     } else {
